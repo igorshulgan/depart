c712c87b1d2c2be5e2a672672893649afdfe4511
igor_shulgan <Igor.shulgan2013@gmail.com>
2016-04-05 22:31:31 +0300

    Added GET for requests

---
+++ src/com/depart/stud/Requests.java
@@ -0,0 +1,45 @@
+package com.depart.stud;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import com.depart.readDB.*;
+import com.google.gson.Gson;
+
+@WebServlet("/Requests")
+public class Requests extends HttpServlet {
+
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    		JSONArray res = null;
+			try {
+				res = GetRequests.get();
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+    		response.getWriter().write(res.toString());
+    }
+
+}

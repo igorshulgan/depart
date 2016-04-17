dc299aab44c37ace38548b30383347abd053c479
igor_shulgan <Igor.shulgan2013@gmail.com>
2016-04-17 12:00:14 +0300

    Added depart for requests

--- src/com/depart/info/Departments.java
+++ src/com/depart/info/Departments.java
@@ -28,7 +28,6 @@ public class Departments extends HttpServlet {
 	 */
 	private static final long serialVersionUID = 1L;
 
-
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
     		JSONArray res = null;
